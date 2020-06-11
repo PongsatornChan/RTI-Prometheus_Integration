6369acfe564d99271e54d1d972566adeddfaa566
PongsatornChan <55928951+PongsatornChan@users.noreply.github.com>
2020-06-10 18:38:52 -0700

    Add file via upload

---
+++ MonitorProcessor.hpp
@@ -0,0 +1,99 @@
+/*
+ * (c) 2018 Copyright, Real-Time Innovations, Inc.  All rights reserved.
+ *
+ * RTI grants Licensee a license to use, modify, compile, and create derivative
+ * works of the Software.  Licensee has the right to distribute object form
+ * only for use with RTI products.  The Software is provided "as is", with no
+ * warranty of any type, including any warranty for fitness for any purpose.
+ * RTI is under no obligation to maintain or support the Software.  RTI shall
+ * not be liable for any incidental or consequential damages arising out of the
+ * use or inability to use the software.
+ */
+
+#ifndef MONITOR_PROCESSOR_HPP_
+#define MONITOR_PROCESSOR_HPP_
+
+#include <iterator>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <chrono>
+#include <map>
+#include <memory>
+#include <string>
+#include <thread>
+
+#include <dds/core/corefwd.hpp>
+
+#include <dds/core/Optional.hpp>
+#include <dds/core/xtypes/DynamicData.hpp>
+#include <dds/core/xtypes/StructType.hpp>
+#include <rti/routing/processor/Processor.hpp>
+#include <rti/routing/processor/ProcessorPlugin.hpp>
+
+/*
+* library from Prometheus C++ Client library
+*/
+#include <prometheus/counter.h>
+#include <prometheus/exposer.h>
+#include <prometheus/registry.h>
+
+using namespace prometheus;
+
+
+class MonitorExposer : public rti::routing::processor::NoOpProcessor {
+public:
+    void on_data_available(rti::routing::processor::Route &) override;
+
+    void on_input_enabled(
+            rti::routing::processor::Route &route,
+            rti::routing::processor::Input &input) override;
+
+    MonitorExposer();
+
+    ~MonitorExposer();
+
+private:
+    // Optional member for deferred initialization: this object can be created
+    // only when the output is enabled.
+    // You can use std::optional if supported in your platform
+    dds::core::optional<dds::core::xtypes::DynamicData> output_data_;
+
+    // Prometheus components
+    // prometheus::Exposer exposer{"127.0.0.1:8080", 1};
+    // std::shared_ptr<Registry> registry;
+    prometheus::Family<prometheus::Counter>& counter_family;
+    prometheus::Counter& second_counter;
+    prometheus::Family<prometheus::Gauge>& gauge_family;
+    prometheus::Gauge& current_pushed_samples;
+    prometheus::Gauge& current_pushed_bytes;
+};
+
+class MonitorProcessorPlugin : public rti::routing::processor::ProcessorPlugin {
+public:
+
+    rti::routing::processor::Processor *create_processor(
+            rti::routing::processor::Route &route,
+            const rti::routing::PropertySet &properties) override;
+
+    void delete_processor(
+            rti::routing::processor::Route &route,
+            rti::routing::processor::Processor *processor) override;
+
+    MonitorProcessorPlugin(const rti::routing::PropertySet &properties);
+};
+
+
+/**
+ * This macro defines a C-linkage symbol that can be used as create function
+ * for plug-in registration through XML.
+ *
+ * The generated symbol has the name:
+ *
+ * \code
+ * ShapesProcessorPlugin_create_processor_plugin
+ * \endcode
+ */
+RTI_PROCESSOR_PLUGIN_CREATE_FUNCTION_DECL(MonitorProcessorPlugin);
+
+#endif
